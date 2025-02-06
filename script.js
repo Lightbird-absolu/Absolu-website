7d94824ccb4128cd82b560f2fb6ea1030a52c6fb
Lightbird-absolu <nathanhess07@gmail.com>
2025-02-06 23:09:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector("button");
+    button.addEventListener("click", function() {
+        alert("Le jeu n'est pas encore disponible. Restez à l'écoute !");
+    });
+});
